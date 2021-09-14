251793a83a00d8c572cca5a7449edfa62f7a073a
AlexeyVolchenkov <72989930+AlexeyVolchenkov@users.noreply.github.com>
2021-09-14 23:18:33 +0300

    lk2

---
+++ lk2/task_lk2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+class FooString
+{
+	char* buf;
+	char* buf1;
+
+public:
+	FooString(char*);
+	void show();
+	bool compare(FooString);
+};
+
+FooString::FooString(char* buf, char* buf1)
+
+{
+	this->buf = buf;
+	this->buf1 = buf1;
+}
+
+void FooString::show()
+{
+	cout << buf;
+	cout << buf1;
+}
+
+bool FooString::compare(FooString str)
+{
+	cin >> buf;
+	cin >> buf1;
+	int i = 0;
+
+	while (buf[i] != '\0' && buf1[i] != '\0')
+	{
+		if (buf[i] != buf1[i]) return false;
+		i++;
+	}
+
+	if (buf[i] != buf1[i]) return false;
+
+	return true;
+}
+
+
+int main()
+{
+	char ch1[100], ch2[100];
+	FooString str(ch1);
+	str.show();
+}
